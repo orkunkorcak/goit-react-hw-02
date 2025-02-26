8547a9cd3520f603a51770387212007a2a4ecef3
your_name <orkunkorcak@gmail.com>
2025-02-26 22:18:02 +0300

    hw finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Description from "./components/description/Description";
 import Options from "./components/options/Options";
 import Feedback from "./components/feedback/Feedback";
 
 const App = () => {
-  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem("feedback");
+    return savedFeedback
+      ? JSON.parse(savedFeedback)
+      : { good: 0, neutral: 0, bad: 0 };
+  });
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
 
   const updateFeedback = (feedbackType) => {
     setFeedback((prevFeedback) => ({
--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -1,5 +1,7 @@
 
 const Feedback = ({ feedback }) => {
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const forPercent = feedback.good + feedback.bad;
   if (!feedback.good && !feedback.neutral && !feedback.bad) {
     return <p>No feedback yet</p>;
   }
@@ -8,8 +10,8 @@ const Feedback = ({ feedback }) => {
       <p>Good: {feedback.good}</p>
       <p>Neutral: {feedback.neutral}</p>
       <p>Bad: {feedback.bad}</p>
-      <p>Total: {feedback.good + feedback.neutral + feedback.bad}</p>
-      <p></p>
+      <p>Total: {totalFeedback}</p>
+      <p>Positive: {Math.round((feedback.good / forPercent) * 100)}%</p>
     </div>
   );
 };
